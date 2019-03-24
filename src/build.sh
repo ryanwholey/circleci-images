3cd0b8a4c1fd4913eac7962ce79fd3577d4ca4d1
Ryan Wholey <rjwholey@gmail.com>
2019-03-23 19:49:06 -0700

    pass docker pass via stdin

--- src/build.sh
+++ src/build.sh
@@ -2,6 +2,6 @@
 
 set -euf -o pipefail
 
-docker login -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}"
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker build . -t "${DOCKER_USERNAME}/${CIRCLE_PROJECT_REPONAME}-test:${CIRCLE_SHA1}"
 docker push "${DOCKER_USERNAME}/${CIRCLE_PROJECT_REPONAME}-test:${CIRCLE_SHA1}"
--- src/publish.sh
+++ src/publish.sh
@@ -2,7 +2,7 @@
 
 set -euf -o pipefail
 
-docker login -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}"
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker pull "${DOCKER_USERNAME}/${CIRCLE_PROJECT_REPONAME}-test:${CIRCLE_SHA1}"
 docker tag "${DOCKER_USERNAME}/${CIRCLE_PROJECT_REPONAME}-test:${CIRCLE_SHA1}" "${DOCKER_USERNAME}/${CIRCLE_PROJECT_REPONAME}:${VERSION}"
 docker push "${DOCKER_USERNAME}/${CIRCLE_PROJECT_REPONAME}:${VERSION}"
